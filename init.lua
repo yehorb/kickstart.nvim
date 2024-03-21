c58300f8000512e49e048eef565b8f7658062247
Yehor Borkov <yehor.borkov@gmail.com>
2024-03-21 23:46:56 +0200

    fix: tweak plugins

--- init.lua
+++ init.lua
@@ -89,7 +89,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 -- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
@@ -98,37 +98,15 @@ end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 -- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
+
 require('lazy').setup({
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
-  -- NOTE: Plugins can also be added by using a table,
-  -- with the first argument being the link and the following
-  -- keys can be used to configure plugin behavior/loading/etc.
-  --
-  -- Use `opts = {}` to force a plugin to be loaded.
-  --
-  --  This is equivalent to:
-  --    require('Comment').setup({})
+  -- NOTE: Use `opts = {}` to force a plugin to be loaded.
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
-  -- Here is a more advanced example where we pass configuration
-  -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
-  --    require('gitsigns').setup({ ... })
-  --
-  -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
@@ -140,22 +118,22 @@ require('lazy').setup({
         changedelete = { text = '~' },
       },
     },
+    keys = {
+      { '<leader>g', desc = '[G]it' },
+      { ']g', function() require('gitsigns').next_hunk() end, desc = 'Next Git hunk' },
+      { '[g', function() require('gitsigns').prev_hunk() end, desc = 'Previous Git hunk' },
+      { '<leader>gl', function() require('gitsigns').blame_line() end, desc = 'View Git blame' },
+      { '<leader>gL', function() require('gitsigns').blame_line { full = true } end, desc = 'View full Git blame' },
+      { '<leader>gp', function() require('gitsigns').preview_hunk() end, desc = 'Preview Git hunk inline' },
+      { '<leader>gP', function() require('gitsigns').preview_hunk() end, desc = 'Preview Git hunk' },
+      { '<leader>gh', function() require('gitsigns').reset_hunk() end, desc = 'Reset Git hunk' },
+      { '<leader>gr', function() require('gitsigns').reset_buffer() end, desc = 'Reset Git buffer' },
+      { '<leader>gs', function() require('gitsigns').stage_hunk() end, desc = 'Stage Git hunk' },
+      { '<leader>gS', function() require('gitsigns').stage_buffer() end, desc = 'Stage Git buffer' },
+      { '<leader>gu', function() require('gitsigns').undo_stage_hunk() end, desc = 'Unstage Git hunk' },
+      { '<leader>gd', function() require('gitsigns').diffthis() end, desc = 'View Git diff' },
+    },
   },
-
-  -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
-  --
-  -- This is often very useful to both group configuration, as well as handle
-  -- lazy loading plugins that don't need to be loaded immediately at startup.
-  --
-  -- For example, in the following configuration, we use:
-  --  event = 'VimEnter'
-  --
-  -- which loads which-key before all the UI elements are loaded. Events can be
-  -- normal autocommands events (`:help autocmd-events`).
-  --
-  -- Then, because we use the `config` key, the configuration only runs
-  -- after the plugin has been loaded:
-  --  config = function() ... end
 
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
@@ -174,13 +152,6 @@ require('lazy').setup({
     end,
   },
 
-  -- NOTE: Plugins can specify dependencies.
-  --
-  -- The dependencies are proper plugin specifications as well - anything
-  -- you do for a plugin at the top level, you can do for a dependency.
-  --
-  -- Use the `dependencies` key to specify the dependencies of a particular plugin
-
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
@@ -189,14 +160,9 @@ require('lazy').setup({
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
         'nvim-telescope/telescope-fzf-native.nvim',
-
-        -- `build` is used to run some command when the plugin is installed/updated.
-        -- This is only run then, not every time Neovim starts up.
-        build = 'make',
-
-        -- `cond` is a condition used to determine whether this plugin should be
-        -- installed and loaded.
-        cond = function() return vim.fn.executable 'make' == 1 end,
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
+        -- TODO: `libfzf.dll` in the plugin direcotory
+        cond = function() return vim.fn.executable 'cmake' == 1 end,
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },
 
