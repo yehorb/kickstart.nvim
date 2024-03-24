f4794ed05bb1555813f9054cb769d6f5e50d6f9e
Yehor Borkov <yehor.borkov@gmail.com>
2024-03-24 12:22:40 +0200

    fix: tweak shell options

--- init.lua
+++ init.lua
@@ -65,9 +65,9 @@ vim.opt.fileformats = { 'unix', 'dos' }
 -- Set PowerShell Core as the default shell
 vim.cmd [[
 let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
-let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';'
+let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
 let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
-let &shellpipe  = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
+let &shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
 set shellquote= shellxquote=
 ]]
 
