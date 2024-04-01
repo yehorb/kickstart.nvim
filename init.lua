44c14ccdea8e78c97bc1db131400738e21258609
Yehor Borkov <yehor.borkov@gmail.com>
2024-04-01 11:54:33 +0300

    feat: add yaml utils

--- init.lua
+++ init.lua
@@ -456,6 +456,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'prettierd',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -496,6 +497,7 @@ require('lazy').setup({
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         -- javascript = { { "prettierd", "prettier" } },
+        yaml = { { 'prettierd', 'prettier' } },
       },
     },
   },
