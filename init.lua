c9f94db0bf78260c86b45291b1cc683d7d4c3148
Yehor Borkov <yehor.borkov@gmail.com>
2024-04-01 13:44:11 +0300

    feat: add lazy keymaps

--- init.lua
+++ init.lua
@@ -133,6 +133,11 @@ if not vim.loop.fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
+-- [[ Lazy Keymaps ]]
+
+vim.keymap.set('n', '<leader>pp', function() require('lazy').home() end, { desc = 'Plugins Status' })
+vim.keymap.set('n', '<leader>pS', function() require('lazy').sync() end, { desc = 'Plugins Sync' })
+
 -- [[ Configure and install plugins ]]
 
 require('lazy').setup({
@@ -182,6 +187,7 @@ require('lazy').setup({
       require('which-key').register {
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
         ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+        ['<leader>p'] = { name = '[P]ackages', _ = 'which_key_ignore' },
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
