121217ca7e17c54ed96089bda33ae1fb5a3bdd6f
Yehor Borkov <yehor.borkov@gmail.com>
2024-05-20 14:28:15 +0300

    refactor: clean up lsp init

--- init.lua
+++ init.lua
@@ -444,6 +444,8 @@ require('lazy').setup({
             },
           },
         },
+        prettierd = {},
+        stylua = {}, -- Used to format Lua code
         yamlls = {
           settings = {
             yaml = {
@@ -467,10 +469,6 @@ require('lazy').setup({
       -- You can add other tools here that you want Mason to install
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-        'prettierd',
-      })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
